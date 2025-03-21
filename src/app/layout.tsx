a2903f498a77e003a0f74e08701277f8c2fb818d
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-21 07:13:53 +0700

    refactor: fixed build for ci

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Cinzel } from 'next/font/google';
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
 
 const cinzel = Cinzel({
